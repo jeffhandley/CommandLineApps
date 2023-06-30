fa3db33c1e19dd48a1c461e9ac68c39a8e0fd042
Jeff Handley <jeffhandley@users.noreply.github.com>
2023-06-30 11:44:15 -0700

    Add a config-less console app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.CommandLine;
+
+// apply-labels --org dotnet --repo runtime --issue 40074 area-System.Security untriaged --dry-run
+var cli = CliParser.Parse(args);
+string org = cli.GetOption<string>("org");
+string repo = cli.GetOption<string>("repo");
+int? issue = cli.GetOption<int?>("issue");
+int? pr = cli.GetOption<int?>("pr");
+IEnumerable<string> labels = cli.GetArguments<string>();
+bool isDryRun = cli.HasOption("dry-run");
+
+if (issue is not null)
+{
+    // Add the labels to the specified issues
+}
+else if (pr is not null)
+{
+    // Add the labels to the specified PR
+}
---
+++ ConsoleApp1/System.CommandLine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.CommandLine
+{
+    public class CliParser
+    {
+        public static CliParseResult Parse(string[] args)
+        {
+            return default(CliParseResult);
+        }
+    }
+
+    public struct CliParseResult
+    {
+        public T GetOption<T>(string name)
+        {
+            return default(T)!;
+        }
+
+        public bool HasOption(string name)
+        {
+            return false;
+        }
+
+        public IEnumerable<T> GetArguments<T>()
+        {
+            return Enumerable.Empty<T>();
+        }
+    }
+}
