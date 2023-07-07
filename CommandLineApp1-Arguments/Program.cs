693cf84b4cb4d38147d2c13d888a678bfaa870f1
Jeff Handley <jeffhandley@users.noreply.github.com>
2023-07-07 13:08:36 -0700

    Illustrate a ParseArguments API

---
+++ CommandLineApp1-Arguments/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.CommandLine;
+
+// apply-labels dotnet runtime issue 40074 area-System.Security untriaged --dry-run
+var cmd = CliParser.ParseArguments(args);
+var org = cmd.GetArgument<string>();
+var repo = cmd.GetArgument<string>();
+var type = cmd.GetArgument<IssueOrPr>();
+var number = cmd.GetArgument<int>();
+var labels = cmd.GetArguments<string>(minArgs: 1);
+var dryrun = false;
+
+if (labels.Contains("--dry-run", StringComparer.OrdinalIgnoreCase))
+{
+    dryrun = true;
+    labels = labels.Where(l => !string.Equals(l, "--dry-run", StringComparison.OrdinalIgnoreCase));
+}
+
+if (type == IssueOrPr.Issue)
+{
+    // Add the labels to the specified issues
+}
+else
+{
+    // Add the labels to the specified PR
+}
+
+public enum IssueOrPr
+{
+    Issue,
+    PR
+}
---
+++ CommandLineApp1-Arguments/System.CommandLine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.CommandLine
+{
+    public static partial class CliParser
+    {
+        public static CliParseResult ParseArguments(string[] args) => default;
+    }
+
+    public partial struct CliParseResult
+    {
+        public T GetArgument<T>() => default!;
+        public IEnumerable<T> GetArguments<T>(ushort minArgs = 0, ushort maxArgs = 0) => Enumerable.Empty<T>();
+    }
+}
