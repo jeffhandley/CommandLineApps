791ba91da08d798b2d36790cbb30a14b3e21793a
Jeff Handley <jeffhandley@users.noreply.github.com>
2023-08-06 23:33:26 -0700

    Simplify the S.CL type-inferred demo

--- 1-Single_Command/f-System.CommandLine-TypeInferred/1f-System.CommandLine-Single_Command-TypeInferred.cs
+++ 1-Single_Command/f-System.CommandLine-TypeInferred/1f-System.CommandLine-Single_Command-TypeInferred.cs
@@ -1,33 +1,21 @@
 ﻿// apply-labels --org dotnet --repo runtime --issue 40074 area-System.Security untriaged --dry-run
 if (args.Length == 0) args = new[] { "--org", "dotnet", "--repo", "runtime", "--issue", "40074", "area-System.Security", "untriaged", "--dry-run" };
 
-var org = new CliOption<string>("--org", "-o") { Required = true };
-var repo = new CliOption<string>("--repo", "-r") { Required = true };
-var issue = new CliOption<int?>("--issue", "-i");
-var pr = new CliOption<int?>("--pr", "-p");
-var labels = new CliArgument<string[]>("labels") { Arity = ArgumentArity.OneOrMore };
-var dryrun = new CliOption<bool>("--dry-run", "-d");
-
-var cli = new CliRootCommand { org, repo, issue, pr, labels, dryrun };
-cli.Validators.Add(result =>
-{
-    if (result.GetValue(issue) is null && result.GetValue(pr) is null)
-    {
-        result.AddError("Either --issue or --pr must be specified");
-    }
-});
-
-cli.SetAction((ParseResult cmd) =>
-{
-    if ((int?)cmd[issue] is not null)
-    {
-        GitHubHelper.Labels.AddToIssue(cmd[org], cmd[repo], cmd[issue], (string[])cmd[labels], cmd[dryrun]);
-    }
-    else if ((int?)cmd[pr] is not null)
-    {
-        GitHubHelper.Labels.AddToPullRequest(cmd[org], cmd[repo], cmd[issue], (string[])cmd[labels], cmd[dryrun]);
-    }
-});
+var cli = new Cli();
+var org = cli.AddOption<string>("org", required: true);
+var repo = cli.AddOption<string>("repo", required: true);
+var issue = cli.AddOption<int?>("issue");
+var pr = cli.AddOption<int?>("pr");
+var labels = cli.AddArgument<string[]>();
+var dryrun = cli.AddOption<bool>("dry-run");
 
 var cmd = cli.Parse(args);
-return cmd.Invoke();
+
+if ((int?)cmd[issue] is not null)
+{
+    GitHubHelper.Labels.AddToIssue(cmd[org], cmd[repo], cmd[issue], (string[])cmd[labels], cmd[dryrun]);
+}
+else if ((int?)cmd[pr] is not null)
+{
+    GitHubHelper.Labels.AddToPullRequest(cmd[org], cmd[repo], cmd[pr], (string[])cmd[labels], cmd[dryrun]);
+}
--- 1-Single_Command/f-System.CommandLine-TypeInferred/1f-System.CommandLine-shim.cs
+++ 1-Single_Command/f-System.CommandLine-TypeInferred/1f-System.CommandLine-shim.cs
@@ -1,8 +1,28 @@
-﻿public class CliRootCommand : System.CommandLine.CliRootCommand
+﻿using System.Collections.ObjectModel;
+using System.CommandLine.Parsing;
+
+public class Cli : System.CommandLine.CliRootCommand
 {
-    public void SetAction(Action<ParseResult> action)
+    public ParseResult Parse(string[] args)
     {
-        base.SetAction(result => action(new(result)));
+        return new ParseResult(base.Parse(args));
+    }
+
+    public CliOption<T> AddOption<T>(string name, bool required = false)
+    {
+        var option = new CliOption<T>($"--{name}", new[] { $"-{name[0]}" }) { Required = required };
+        Options.Add(option);
+
+        return option;
+    }
+
+    public CliArgument<T> AddArgument<T>()
+    {
+        var argName = $"arg{Arguments.Count}";
+        var argument = new CliArgument<T>(argName);
+        Arguments.Add(argument);
+
+        return argument;
     }
 }
 
@@ -11,47 +31,47 @@ public class ParseResult
     private System.CommandLine.ParseResult _result;
     public ParseResult(System.CommandLine.ParseResult result) => _result = result;
 
-    public OptionValueGetter this[CliOption option] => new(_result, option);
-    public ArgumentValueGetter this[CliArgument argument] => new(_result, argument);
+    public OptionValueGetter this[CliOption option] => new(_result.GetResult(option), option);
+    public ArgumentValueGetter this[CliArgument argument] => new(_result.GetResult(argument), argument);
 
     public struct OptionValueGetter
     {
-        private System.CommandLine.ParseResult _result;
+        private OptionResult? _result;
         private CliOption _option;
 
-        internal OptionValueGetter(System.CommandLine.ParseResult result, CliOption option)
+        internal OptionValueGetter(OptionResult? result, CliOption option)
         {
             _result = result;
             _option = option;
         }
 
-        public static implicit operator bool(OptionValueGetter getter) => getter._result.GetValue((CliOption<bool>)getter._option);
-        public static implicit operator string(OptionValueGetter getter) => getter._result.GetValue((CliOption<string>)getter._option)!;
-        
-        public static implicit operator int(OptionValueGetter getter)
+        public static T EnforceRequired<T>(CliOption option)
         {
-            if (getter._option is CliOption<int?>)
+            if (option.Required)
             {
-                return getter._result.GetValue((CliOption<int?>)getter._option)!.Value;
+                throw new ArgumentNullException(option.Name);
             }
 
-            return getter._result.GetValue((CliOption<int>)getter._option);
+            return default!;
         }
 
-        public static implicit operator int?(OptionValueGetter getter) => getter._result.GetValue<int?>((CliOption<int?>)getter._option);
+        public static implicit operator bool(OptionValueGetter getter) => getter._result?.GetValueOrDefault<bool>() ?? EnforceRequired<bool>(getter._option);
+        public static implicit operator string(OptionValueGetter getter) => getter._result?.GetValueOrDefault<string>() ?? EnforceRequired<string>(getter._option);
+        public static implicit operator int?(OptionValueGetter getter) => getter._result?.GetValueOrDefault<int?>() ?? EnforceRequired<int?>(getter._option);
+        public static implicit operator int(OptionValueGetter getter) => getter._result?.GetValueOrDefault<int>() ?? EnforceRequired<int>(getter._option);
     }
 
     public struct ArgumentValueGetter
     {
-        private System.CommandLine.ParseResult _result;
+        private ArgumentResult? _result;
         private CliArgument _argument;
 
-        internal ArgumentValueGetter(System.CommandLine.ParseResult result, CliArgument argument)
+        internal ArgumentValueGetter(ArgumentResult? result, CliArgument argument)
         {
             _result = result;
             _argument = argument;
         }
 
-        public static implicit operator string[](ArgumentValueGetter getter) => getter._result.GetValue<string[]>((CliArgument<string[]>)getter._argument)!;
+        public static implicit operator string[](ArgumentValueGetter getter) => getter._result?.GetValueOrDefault<string[]>() ?? default!;
     }
 }
